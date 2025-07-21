ec967b09b683f6500ed7f47d071fcb089cba069e
RC Prasad <rcprasad.2000@gmail.com>
2025-07-21 16:45:52 +0530

    added html

--- index.js
+++ index.js
@@ -1,24 +1,65 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = "harkirat123";
+const cors = require("cors"); // <-- add this line
+
+const JWT_SECRET = "kirat123123";
 
 const app = express();
-app.use (express.json()); // this liine of code help u to extract the body that is present in the json format 
+app.use(cors()); // <-- add this line
+app.use(express.json());
 
 const users = [];
 
-// 1. Logger middleware function
 function logger(req, res, next) {
     console.log(req.method + " request came");
     next();
 }
 
-// 5. Auth middleware function
+// localhost:3000
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.post("/signup", logger, function(req, res) {
+    const username = req.body.username
+    const password = req.body.password
+    users.push({
+        username: username,
+        password: password
+    })
+
+    // we should check if a user with this username already exists
+
+    res.json({
+        message: "You are signed in"
+    })
+})
+
+app.post("/signin", logger, function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let foundUser = null;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username && users[i].password === password) {
+            foundUser = users[i];
+        }
+    }
+
+    if (!foundUser) {
+        return res.status(401).json({ message: "Credentials incorrect" });
+    }
+
+    const token = jwt.sign({ username: foundUser.username }, JWT_SECRET);
+    res.json({ token: token });
+});
+
 function auth(req, res, next) {
     const token = req.headers.token;
     const decodedData = jwt.verify(token, JWT_SECRET);
 
     if (decodedData.username) {
+        // req = {status, headers...., username, password, userFirstName, random; ":123123"}
         req.username = decodedData.username
         next()
     } else {
@@ -28,80 +69,27 @@ function auth(req, res, next) {
     }
 }
 
-app.post("/signup",logger,function(req,res){
-
-        const username = req.body.username
-        const password = req.body.password
-
-        users.push({
-            username : username,
-            password : password
-        })
-// we should checkk whether the username is already exist or not 
-        res.json({
-            message:"You are Signed In"
-        })
-})
-
-app.post("/signin", logger, function(req,res){
-
-    const username = req.body.username;
-    const password = req.body.password;
-
-    let foundUser = null;
-
-    for (let i = 0 ; i < users.length; i++){
-        if(users[i].username === username && users[i].password === password) { 
-            foundUser = users[i];
-
-        }
-    }
-
-    if(!foundUser) {
-        res.json({
-                message : "Credentials incorrect"
-        })
-        return
-    } else {
-        const token = jwt.sign({
-            username
-        },JWT_SECRET);
-
-        // 4. Set custom headers in /signin
-        res.header("jwt", token);
-        res.header("random", "harkirat");
-
-        res.json({
-            token: token
-        })
-    }
-    
-
-    
-
-})
-
-app.get("/me", logger, auth, function(req,res){
-
+app.get("/me", logger, auth, function(req, res) {
     const currentUser = req.username;
+    let foundUser = null; // <-- declare foundUser
 
-    let foundUser = null;
-
-    for (let i = 0 ; i < users.length; i++){  //decoding happens thrugh verify of tokken u will be decoded and get u the username 
-        if(users[i].username === currentUser) { 
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === currentUser) {
             foundUser = users[i];
-
         }
     }
-    res.json({
-        username : foundUser.username,
-        password:foundUser.password
-    })
-});
 
-// 3. Serve index.html on GET /
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/public/index.html");
+    if (!foundUser) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({
+        username: foundUser.username,
+        password: foundUser.password
+    });
 });
 
 app.listen(3000);
+
+// To check which process is using port 3000, run the following command in your terminal:
+// netstat -ano | findstr :3000
